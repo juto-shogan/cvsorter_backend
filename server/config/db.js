ab6afe9c5302b29e75dbf9996e57ffab3089487d
juto-shogan <somtombonu53@gmail.com>
2025-07-21 11:40:02 +0100

    modification: changed require to import

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,15 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // server/config/db.js
 const connectDB = async () => {
   try {
-    
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected successfully: ${conn.connection.host}`);
-
   } catch (error) {
+  // Handle connection errors
     console.error('MongoDB connection error:', error);
   }
-
 }
 
-module.exports = connectDB;
-
+export default connectDB;
