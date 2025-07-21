bf56b51aebf964aace075e21ca6d7668d07bdfa5
juto-shogan <somtombonu53@gmail.com>
2025-07-21 17:19:29 +0100

    created: authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import UserModel from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+export const authenticateUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await UserModel.findById(decodedToken.userId);
+      req.user = user;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalid or expired token",
+      });
+    }
+  } else {
+    return res.status(401).json({
+      success: false,
+      error: "Authorization header is required",
+    });
+  }
+};
